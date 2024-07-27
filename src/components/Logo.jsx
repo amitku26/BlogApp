299121777860a10a94903ff1e4009c89776214a8
amitkumar2026 <amitkumar829448@gmail.com>
2024-07-28 00:57:32 +0530

    final submision

---
+++ src/components/Logo.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+function Logo() {
+  return (
+    <div>
+      {/* <img src="/Logo.jpeg" alt="Logo" className='w-10 h-10 rounded-md'/> */}
+      <h1>Log</h1>
+    </div>
+  )
+}
+
+export default Logo
